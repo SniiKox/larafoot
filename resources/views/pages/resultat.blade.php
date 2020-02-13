3d22f6b5a5305f2398a12a34d138480b5fbcd53d
Adrien <adrienhuchet@hotmail.com>
2020-02-13 12:18:13 +0100

    Modification mineure - design de la page résultat

--- resources/views/pages/resultat.blade.php
+++ resources/views/pages/resultat.blade.php
@@ -1,6 +1,14 @@
 @extends('default')
 
 @section('content')
-    <h1> Page de resultat </h1>
+    <div class="bloc-analyse">
+        <div class="bloc-txt">
+            <div class="title-analyse">Résultat</div>
+            <p>Retrouvez tous les résultats de votre équipe !</p>
+            <p>En partenariat avec FLASH RÉSULTATS</p>
+            <button>Consulter les scores</button>
+        </div>
+        <img class="bloc-txt" src="img/resultat.jpg">
+    </div>
 @endsection
 
