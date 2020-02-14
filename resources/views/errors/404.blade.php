f88cb90a9b8e18fc8d373e5467bd5d3fb33020ad
Guillaume Charpentier <guillaume.charpentier@imie.fr>
2020-02-14 16:25:37 +0100

    Modification mineure - Gestion des erreurs 500/404
    Pages personnalisées créées pour les erreurs de type 500 et 404.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('default')
+
+@section('content')
+  <div class="bloc-analyse">
+      <div class="bloc-txt">
+          <div class="title-analyse">
+              Cette page n'existe pas !
+          </div>
+      </div>
+  </div>
+@endsection
+
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+@extends('default')
+
+@section('content')
+  <div class="bloc-analyse">
+      <div class="bloc-txt">
+          <div class="title-analyse">
+              Un problème a été détecté ! Revenez un peu plus tard !
+          </div>
+      </div>
+  </div>
+@endsection
+
+</html>
