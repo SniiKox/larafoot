dcbd84f878fb6081679077ec174bd9e6259cd361
Guillaume Charpentier <guillaume.charpentier@imie.fr>
2020-03-12 16:42:57 +0100

    Modification mineure - FIX javaScript

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,6 @@ $(function(){
         var hash = this.hash;
 
         $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
-
     });
 
 })
-
--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="js/script.js"></script>
+
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -84,9 +84,10 @@
     @yield('content')
 
 <script src="//code.jquery.com/jquery.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="js/script.js"></script>
+
 @include('flashy::message')
 
 </body>
